0289f58b917fc314542c226d9ee857c2c68bdd9f
Bright Munemu <brightmunemu@gmail.com>
2026-01-21 08:07:12 +0100

    create functions

--- js/todo.js
+++ js/todo.js
@@ -15,5 +15,28 @@ function displayTasks() {
 
 //Add task
 function addTask() {
-    
+  let taskInput = document.querySelector("#input-value");
+  let text = taskInput.value;
+  if (text === "") {
+    alert("Input cannot be empty");
+    return;
+  }
+  tasks.push(test);
+  taskInput.value = "";
+  saveTask();
+  displayTasks();
+}
+
+// Remove task
+function removeTask(i) {
+  tasks.splice(i, 1);
+  saveTask();
+  displayTasks();
+}
+
+// Clear all task
+function clearAllTask(i) {
+  tasks = [];
+  saveTask();
+  displayTasks();
 }
